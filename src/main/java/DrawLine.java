5f52979d52e7d43a5a3f4b4fe3f6d8d77b4f3aa9
richardgeorgepadua <richardpadua555@gmail.com>
2024-12-16 16:48:39 -0500

    Drawing a line - Implemented

---
+++ src/main/java/DrawLine.java
@@ -0,0 +1,10 @@
+public class DrawLine {
+    public static void main(String[] args) {
+        String line = "_";
+        int lineRepeatCount = 20;
+
+        for(int i=1; i<= lineRepeatCount; i++){
+            System.out.print(line);
+        }
+    }
+}
